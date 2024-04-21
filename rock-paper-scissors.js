4b1923016c6bc0e3f84a33c8f1e22c6b5abf104d
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-21 17:32:01 -0400

    Implemented game to 5 with simple UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,34 @@
+const rockBtn = document.querySelector("#rockBtn")
+const paperBtn = document.querySelector("#paperBtn")
+const scissorsBtn = document.querySelector("#scissorsBtn")
+
+
+rockBtn.addEventListener("click", () => {
+    playRound("rock")
+})
+
+paperBtn.addEventListener("click", () => {
+    playRound("paper")
+})
+
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors")
+})
+
+const roundDiv = document.querySelector("#roundDiv")
+const playerSelectionDiv = document.querySelector("#playerSelectionDiv")
+const computerSelectionDiv = document.querySelector("#computerSelectionDiv")
+const roundResult = document.querySelector("#roundResult")
+const playerScoreDiv = document.querySelector("#playerScoreDiv")
+const computerScoreDiv = document.querySelector("#computerScoreDiv")
+const gameResultDiv = document.querySelector("#gameResultDiv")
+
+let playerScore = 0
+let computerScore = 0
+let round = 1
+
+
+
 function getComputerChoice() {
     choices = ["rock", "paper", "scissors"];
     let randomIndex = Math.floor(Math.random() * choices.length);
@@ -6,75 +37,86 @@ function getComputerChoice() {
 }
 
 
-function playRound(playerSelection = prompt("Choose from: rock, paper, or scissors").toLowerCase(), computerSelection = getComputerChoice()) {
+function restartGame() {
+    round = 1
+    playerScore = 0
+    computerScore = 0
+}
 
-    console.log(`Player choice: ${playerSelection}\nComputer choice: ${computerSelection}`);
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
+    console.log(`Round: ${round}`)
+
+
+    if (round == 1) {
+        gameResultDiv.textContent = ""
+        playerScoreDiv.textContent = `Player score: ${playerScore}`
+        computerScoreDiv.textContent = `Computer score: ${computerScore}`
+    }
+
+    roundDiv.textContent = `Round: ${round}`
+
+    playerSelectionDiv.textContent = `Player selection: ${playerSelection}`
+    computerSelectionDiv.textContent = `Computer selection: ${computerSelection}`
 
     if (playerSelection == computerSelection) {
-        return "Draw."
+        roundResult.textContent = `Result: Draw`
     }
 
     else if (playerSelection == "rock") {
         if (computerSelection == "scissors") {
-            console.log(`${playerSelection} beats ${computerSelection}`);
-            return "Player wins.";
+            roundResult.textContent = `Result: ${playerSelection} beats ${computerSelection}. Player wins.`
+            playerScore++
+            playerScoreDiv.textContent = `Player score: ${playerScore}`
         }
         else {
-            console.log(`${computerSelection} beats ${playerSelection}`);
-            return "Computer wins.";
+            roundResult.textContent = `Result: ${computerSelection} beats ${playerSelection}. Computer wins.`
+            computerScore++
+            computerScoreDiv.textContent = `Computer score: ${computerScore}`
         }
     }
 
     else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            console.log(`${playerSelection} beats ${computerSelection}`)
-            return "Player wins.";
+            roundResult.textContent = `Result: ${playerSelection} beats ${computerSelection}. Player wins.`
+            playerScore++
+            playerScoreDiv.textContent = `Player score: ${playerScore}`
         }
         else {
-            console.log(`${computerSelection} beats ${playerSelection}`);
-            return "Computer wins.";
+            roundResult.textContent = `Result: ${computerSelection} beats ${playerSelection}. Computer wins.`
+            computerScore++
+            computerScoreDiv.textContent = `Computer score: ${computerScore}`
         }
     }
 
     else if (playerSelection == "scissors") {
         if (computerSelection == "paper") {
-            console.log(`${playerSelection} beats ${computerSelection}`)
-            return "Player wins.";
+            roundResult.textContent = `Result: ${playerSelection} beats ${computerSelection}. Player wins.`
+            playerScore++
+            playerScoreDiv.textContent = `Player score: ${playerScore}`
         }
         else {
-            console.log(`${computerSelection} beats ${playerSelection}`);
-            return "Computer wins.";
+            roundResult.textContent = `Result: ${computerSelection} beats ${playerSelection}. Computer wins.`
+            computerScore++
+            computerScoreDiv.textContent = `Computer score: ${computerScore}`
         }
     }
+
+    round++
+
+    if (playerScore == 5) {
+        gameResultDiv.textContent = `Player wins the game`
+        restartGame()
+    }
+
+    else if (computerScore == 5) {
+        gameResultDiv.textContent = `Computer wins the game`
+        restartGame()
+    }
+
+    console.log(`Player score: ${playerScore}`)
+    console.log(`Computer score: ${computerScore}`)
+
 }
 
-function playGame() {
-    playerScore = 0;
-    computerScore = 0;
 
-    for (i = 0; i < 5; i++) {
-        let result = playRound();
 
-        if (result == "Player wins.") {
-            playerScore++;
-        }
-
-        else if (result == "Computer wins.") {
-            computerScore++;
-        }
-
-        if (result == "Draw.") {
-            continue;
-        }
-    }
-
-    if (playerScore == computerScore) {
-        return `Player score: ${playerScore}. Computer score: ${computerScore} Draw.`
-    }
-    else if (playerScore > computerScore) {
-        return `Player score: ${playerScore}. Computer score: ${computerScore} Player wins the game.`
-    }
-    else if (playerScore < computerScore) {
-        return `Player score: ${playerScore}. Computer score: ${computerScore}. Computer wins the game.`
-    }
-}
